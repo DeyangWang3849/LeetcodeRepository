f092b4e748d3c37a36e5e22683bc24ab15afd5db
QiankunWANG <qiankunwang3849@163.com>
2019-03-23 21:44:20 +0100

    Runtime from 128ms to 104ms but the room still takes 13.1Mb

--- 002AddTwoNumber.py
+++ 002AddTwoNumber.py
@@ -6,37 +6,36 @@ class ListNode:
 def addTwoNumbers(l1:ListNode,l2:ListNode):
     l1pointer = l1
     l2pointer = l2
-    result = ListNode(l1.val+l2.val)
-    resultpointer = result
-    [c, resultpointer.val] = divmod(resultpointer.val, 10)
-    l1pointer = l1pointer.next
-    l2pointer = l2pointer.next
-    while l1pointer != None and l2pointer != None:
-        resultpointer.next = ListNode(l1pointer.val+l2pointer.val+c)
-        resultpointer = resultpointer.next
-        [c,resultpointer.val] = divmod(resultpointer.val,10)
+    c = 0
+    if l1 == None or l2== None:
+        return None
+    while l1pointer.next != None and l2pointer.next != None:
+        l1pointer.val += l2pointer.val + c
+        c,l1pointer.val =divmod(l1pointer.val,10)
         l1pointer = l1pointer.next
         l2pointer = l2pointer.next
-    while l1pointer != None:
-        resultpointer.next = ListNode(l1pointer.val+c)
-        resultpointer = resultpointer.next
-        [c,resultpointer.val] = divmod(resultpointer.val,10)
-        l1pointer = l1pointer.next
-    while l2pointer != None:
-        resultpointer.next = ListNode(l2pointer.val + c)
-        resultpointer = resultpointer.next
-        [c,resultpointer.val] = divmod(resultpointer.val,10)
-        l2pointer = l2pointer.next
-    if c != 0:
-        resultpointer.next = ListNode(1)
-    return result
 
+    if l2pointer.next != None:
+        l1pointer.next = l2pointer.next
+
+    l1pointer.val += l2pointer.val + c
+    c, l1pointer.val = divmod(l1pointer.val, 10)
+
+    while l1pointer.next != None:
+        l1pointer = l1pointer.next
+        l1pointer.val += c
+        c,l1pointer.val = divmod(l1pointer.val, 10)
+
+    if c != 0:
+        l1pointer.next = ListNode(1)
+    return l1
 if __name__ == '__main__':
     l1 = ListNode(1)
-    # l1.next = ListNode(2)
-    # l1.next.next = ListNode(3)
+    l1.next = ListNode(2)
+    l1.next.next = ListNode(3)
     l2 = ListNode(9)
-    l2.next = ListNode(9)
+    # l2.next = ListNode(9)
+    # l2.next.next = ListNode(9)
     result = addTwoNumbers(l1,l2)
     while result is not None:
         print(result.val)
